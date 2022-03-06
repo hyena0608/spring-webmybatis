819aa4b017ea21e871f89e1265b80330f095c731
hyena0608 <goatsfish@gmail.com>
2022-03-06 23:43:41 +0900

    mybatis 사용해보기 성공

--- src/main/java/com/wizard/pracmybatis/controller/BoardController.java
+++ src/main/java/com/wizard/pracmybatis/controller/BoardController.java
@@ -6,17 +6,19 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/board")
 public class BoardController {
 
     private final BoardService boardService;
 
-    @GetMapping
+    @GetMapping("/list")
     public List<Board> getList() {
         return boardService.getList();
     }
